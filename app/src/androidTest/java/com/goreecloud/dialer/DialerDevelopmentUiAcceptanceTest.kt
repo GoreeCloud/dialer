323243569078a8b38565b3e3b2226071c6e06c5b
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-09-13 20:58:15 -0500

    Fix incoming call access UI acceptance import

--- app/src/androidTest/java/com/goreecloud/dialer/DialerDevelopmentUiAcceptanceTest.kt
+++ app/src/androidTest/java/com/goreecloud/dialer/DialerDevelopmentUiAcceptanceTest.kt
@@ -1,6 +1,5 @@
 package com.goreecloud.dialer
 
-import androidx.compose.ui.test.assertExists
 import androidx.compose.ui.test.assertIsNotEnabled
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
